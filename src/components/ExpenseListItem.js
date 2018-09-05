d03772bf34ddd3a02e0fc3ceb7b6df31027e3c4b
realTrueProger <vovasollo@yandex.ru>
2018-09-05 17:50:30 +0300

    changed date format atListItem component

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import moment from 'moment';
 
 
 const ExpensesListItem = ( { description, amount, createdAt , id}) => (
     <div>
         <h2>Description: {description}</h2>
         <Link to={`/edit/${id}`}>edit</Link>
-        <p>Amount: {amount} CreatedAt: {createdAt}</p>
+        <p>Amount: {amount} CreatedAt: {moment(createdAt).format('MMMM Do YYYY')}</p>
     </div>
 );
 
